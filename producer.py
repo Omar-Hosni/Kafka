1d048e21a3b4cc076a1c31dcb8ca9a8b2a341c89
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-07-29 23:28:42 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,3 +1,17 @@
 from kafka imoprt KafkaProducer
+import json 
+from data import get_register_user
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+def json_serializer(data):
+  return json.dumps(data).encode('utf-8')
+
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                        value_serializer=json_serializer)
+
+if __name__ == '__main__':
+  register_user = get_register_user()
+  js_data = json_serializer(register_user)
+  topic_name = 'etl'
+  producer.send(topic_name, js_data)
+  
